2b1019116654997ede5c495c24bb0a8fbcfd5158
Michael Merickel <michael@merickel.org>
2017-11-26 21:05:11 -0600

    test the request id is set as the thread name

--- tests/test_it.py
+++ tests/test_it.py
@@ -1,3 +1,4 @@
+import logging
 from webob.dec import wsgify
 from webob import Response
 import webtest
@@ -6,7 +7,21 @@ import request_id
 
 @wsgify
 def dummy_app1(request):
-    return Response('foo', status_code=200)
+    logging.info('dummy_app1 test message')
+    return Response(
+        'foo', status_code=200,
+        headers={
+            'xrid': request_id.get_request_id(request),
+        },
+    )
+
+def test_header_in_message(caplog):
+    wrapped_app = request_id.make_filter(dummy_app1, {})
+    app = webtest.TestApp(wrapped_app)
+    response = app.get('/')
+    xrid = response.headers['xrid']
+    rec = [rec for rec in caplog.records if rec.name == 'root'][0]
+    assert 'request={}'.format(xrid) in rec.threadName
 
 def test_source_header(caplog):
     wrapped_app = request_id.make_filter(
